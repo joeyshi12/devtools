47c81ccdafeb21b161c04b4e2aafb81dd3ea10c4
Joey Shi <joeyshi12@gmail.com>
2024-03-03 19:20:29 -0800

    change env name

--- app/database_connection.py
+++ app/database_connection.py
@@ -10,7 +10,7 @@ logger = logging.getLogger("waitress")
 db_config = {
     "host": os.getenv("DB_HOST"),
     "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
+    "password": os.getenv("DB_PASS"),
     "database": os.getenv("DB_NAME")
 }
 
